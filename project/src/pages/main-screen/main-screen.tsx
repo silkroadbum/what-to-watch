f1068c9d051ebbfbefdbb1b372a762935dbb679b
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-01-23 23:25:27 +0600

    Вынес логотип в отдельный компонент

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,4 +1,5 @@
 import FilmCard from '../../components/film-card/film-card';
+import Logo from '../../components/logo/logo';
 
 type MainScreenProps = {
   promoFilmName: string,
@@ -17,13 +18,7 @@ function MainScreen({promoFilmName, promoFilmGenre, promoFilmYear}: MainScreenPr
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header film-card__head">
-          <div className="logo">
-            <a className="logo__link" href="/">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo/>
 
           <ul className="user-block">
             <li className="user-block__item">
--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -1,16 +1,11 @@
 import { Link } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function NotFoundScreen(): JSX.Element {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
-        <div className="logo">
-          <Link to="/" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </Link>
-        </div>
+        <Logo/>
       </header>
 
       <div className="sign-in user-page__content" style={{textAlign: 'center'}}>
