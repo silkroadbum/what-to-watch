2e43364a0d7bb5c96e1939b99de273b16f5398b0
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-01-23 23:06:50 +0600

    Создал компонент ошибки 404 и подключил маршрутизацию в проект

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { AppRoute } from '../../const';
+
 import MainScreen from '../../pages/main-screen/main-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import FilmScreen from '../../pages/film-screen/film-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   promoFilmName: string,
@@ -7,7 +17,19 @@ type AppScreenProps = {
 }
 
 function App({promoFilmName, promoFilmGenre, promoFilmYear}: AppScreenProps): JSX.Element {
-  return <MainScreen promoFilmName = {promoFilmName} promoFilmGenre={promoFilmGenre} promoFilmYear={promoFilmYear}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<MainScreen promoFilmName = {promoFilmName} promoFilmGenre={promoFilmGenre} promoFilmYear={promoFilmYear}/>}/>
+        <Route path={AppRoute.Login} element={<SignInScreen/>}/>
+        <Route path={AppRoute.MyList} element={<MyListScreen/>}/>
+        <Route path={AppRoute.Film} element={<FilmScreen/>}/>
+        <Route path={AppRoute.AddReview} element={<AddReviewScreen/>}/>
+        <Route path={AppRoute.Player} element={<PlayerScreen/>}/>
+        <Route path='*' element={<NotFoundScreen/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
