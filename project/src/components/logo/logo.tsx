2321f1b31f1ff66b8b2f9932b04c74f371f4d10b
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-01-25 16:32:15 +0600

    Скорректировал стили логотипа для футера

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -8,7 +8,7 @@ type LogoProps = {
 function Logo({isFooter}: LogoProps): JSX.Element {
   return (
     <div className="logo">
-      <Link to={AppRoute.Root} className={`logo__link${isFooter ? 'logo__link--light' : ''}`}>
+      <Link to={AppRoute.Root} className={`logo__link ${isFooter ? 'logo__link--light' : ''}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
