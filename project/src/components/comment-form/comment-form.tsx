a8df7e75a026b7f6cebe604629b56ac5db552e63
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-01-25 22:15:13 +0600

    Отрисовал звезды рейтинга через map в компоненте формы

--- project/src/components/comment-form/comment-form.tsx
+++ project/src/components/comment-form/comment-form.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import { ChangeEvent, FormEvent, useState } from 'react';
+
+import { filmRatings } from '../../const';
 
 function CommentForm(): JSX.Element {
   const [formData, setFormData] = useState({
@@ -26,35 +29,12 @@ function CommentForm(): JSX.Element {
     <form onSubmit={onSubmitForm} action="#" className="add-review__form">
       <div className="rating">
         <div className="rating__stars">
-          <input className="rating__input" id="star-10" type="radio" name="rating" value="10" onChange={ratingChangeHandle} checked={formData.rating === 10}/>
-          <label className="rating__label" htmlFor="star-10">Rating 10</label>
-
-          <input className="rating__input" id="star-9" type="radio" name="rating" value="9" onChange={ratingChangeHandle} checked={formData.rating === 9}/>
-          <label className="rating__label" htmlFor="star-9">Rating 9</label>
-
-          <input className="rating__input" id="star-8" type="radio" name="rating" value="8" onChange={ratingChangeHandle} checked={formData.rating === 8}/>
-          <label className="rating__label" htmlFor="star-8">Rating 8</label>
-
-          <input className="rating__input" id="star-7" type="radio" name="rating" value="7" onChange={ratingChangeHandle} checked={formData.rating === 7}/>
-          <label className="rating__label" htmlFor="star-7">Rating 7</label>
-
-          <input className="rating__input" id="star-6" type="radio" name="rating" value="6" onChange={ratingChangeHandle} checked={formData.rating === 6}/>
-          <label className="rating__label" htmlFor="star-6">Rating 6</label>
-
-          <input className="rating__input" id="star-5" type="radio" name="rating" value="5" onChange={ratingChangeHandle} checked={formData.rating === 5}/>
-          <label className="rating__label" htmlFor="star-5">Rating 5</label>
-
-          <input className="rating__input" id="star-4" type="radio" name="rating" value="4" onChange={ratingChangeHandle} checked={formData.rating === 4}/>
-          <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-          <input className="rating__input" id="star-3" type="radio" name="rating" value="3" onChange={ratingChangeHandle} checked={formData.rating === 3}/>
-          <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-          <input className="rating__input" id="star-2" type="radio" name="rating" value="2" onChange={ratingChangeHandle} checked={formData.rating === 2}/>
-          <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-          <input className="rating__input" id="star-1" type="radio" name="rating" value="1" onChange={ratingChangeHandle} checked={formData.rating === 1}/>
-          <label className="rating__label" htmlFor="star-1">Rating 1</label>
+          {filmRatings.map((rating) => (
+            <React.Fragment key={rating}>
+              <input className="rating__input" id={`star-${rating}`} type="radio" name="rating" value={rating} onChange={ratingChangeHandle} checked={formData.rating === rating}/>
+              <label className="rating__label" htmlFor={`star-${rating}`}>Rating {rating}</label>
+            </React.Fragment>)
+          )}
         </div>
       </div>
 
