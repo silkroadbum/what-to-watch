15a7d3176cb1194931ef7346f4119c2991543be2
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-02-01 19:28:14 +0600

    Подключил store через provider на все приложение

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store/index';
 
 import { films } from './mocks/films';
 import { comments } from './mocks/comments';
@@ -17,6 +19,8 @@ const PromoFilm = {
 
 root.render(
   <React.StrictMode>
-    <App promoFilmName={PromoFilm.Name} promoFilmGenre={PromoFilm.Genre} promoFilmYear={PromoFilm.Year} films={films} comments={comments}/>
+    <Provider store={store}>
+      <App promoFilmName={PromoFilm.Name} promoFilmGenre={PromoFilm.Genre} promoFilmYear={PromoFilm.Year} films={films} comments={comments}/>
+    </Provider>
   </React.StrictMode>,
 );
