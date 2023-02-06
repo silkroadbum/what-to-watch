148e8b4084d1854b9b316ec192788fa951aaf4ff
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-02-06 19:27:14 +0600

    Добавил переход в список избранного по икноке аватара пользователя

--- project/src/components/user-block/user-block.tsx
+++ project/src/components/user-block/user-block.tsx
@@ -16,7 +16,9 @@ function UserBlock() {
       <ul className="user-block">
         <li className="user-block__item">
           <div className="user-block__avatar">
-            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            <Link to={AppRoute.MyList}>
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            </Link>
           </div>
         </li>
         <li className="user-block__item">
