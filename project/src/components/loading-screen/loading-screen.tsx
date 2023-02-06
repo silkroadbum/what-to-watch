b9ce23338e47147b733088eb05088878725694bb
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-02-06 20:47:57 +0600

    Скорректировал экран загрузки и обновил инфу по промофильму

--- project/src/components/loading-screen/loading-screen.tsx
+++ project/src/components/loading-screen/loading-screen.tsx
@@ -1,6 +1,27 @@
+import Logo from '../logo/logo';
+import UserBlock from '../user-block/user-block';
+
 function LoadingScreen(): JSX.Element {
   return (
-    <p style={{textAlign: 'center'}}>Loading ...</p>
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <Logo isFooter={false}/>
+
+        <UserBlock/>
+      </header>
+
+      <div className="user-page__content">
+        <p style={{textAlign: 'center'}}>Загрузка...</p>
+      </div>
+
+      <footer className="page-footer">
+        <Logo isFooter/>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
   );
 }
 
--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -31,7 +31,7 @@ function MainScreen(): JSX.Element {
       </Helmet>
       <section className="film-card">
         <div className="film-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+          <img src={promoFilm?.backgroundImage} alt={promoFilm?.name} />
         </div>
 
         <h1 className="visually-hidden">WTW</h1>
@@ -45,7 +45,7 @@ function MainScreen(): JSX.Element {
         <div className="film-card__wrap">
           <div className="film-card__info">
             <div className="film-card__poster">
-              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+              <img src={promoFilm?.previewImage} alt={promoFilm?.name} width="218" height="327" />
             </div>
 
             <div className="film-card__desc">
