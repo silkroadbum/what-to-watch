57fed2c970378e32fd76950cd8e6e0bc22ca1ff7
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-02-11 18:03:51 +0600

    Реализовал видеоплеер

--- project/src/pages/player-screen/player-screen.tsx
+++ project/src/pages/player-screen/player-screen.tsx
@@ -1,55 +1,51 @@
+import { useEffect, useRef, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useNavigate, useParams } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import Spinner from '../../components/spinner/spinner';
 import { useAppSelector } from '../../hooks';
 import { getFilms } from '../../store/app-data/selectors';
 
 function PlayerScreen(): JSX.Element {
+  const [isLoading, setIsLoading] = useState(true);
   const films = useAppSelector(getFilms);
   const {id} = useParams();
   const navigate = useNavigate();
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   const findedFilm = films.find((obj) => obj.id === Number(id));
 
   const handleClickExit = () => {
-    navigate(AppRoute.Root);
+    navigate(-1);
   };
+
+  useEffect(() => {
+    if (videoRef.current === null) {
+      return;
+    }
+
+    videoRef.current.addEventListener('loadeddata', () => setIsLoading(false));
+
+    videoRef.current.play();
+    videoRef.current.defaultPlaybackRate = 1.0;
+    videoRef.current.controls = true;
+
+  }, [id]);
 
   return (
     <div className="player">
       <Helmet>
         <title>WTW. Видеоплеер.</title>
       </Helmet>
-      <video src="#" className="player__video" poster={findedFilm?.backgroundImage}></video>
+
+      {isLoading && <Spinner /> }
+      <video
+        ref={videoRef}
+        src={findedFilm?.videoLink}
+        className="player__video"
+        poster={findedFilm?.backgroundImage}
+      />
 
       <button onClick={handleClickExit} type="button" className="player__exit">Exit</button>
-
-      <div className="player__controls">
-        <div className="player__controls-row">
-          <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style={{left: '30%'}}>Toggler</div>
-          </div>
-          <div className="player__time-value">1:30:29</div>
-        </div>
-
-        <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
-          <div className="player__name">Transpotting</div>
-
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use xlinkHref="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
-        </div>
-      </div>
     </div>
   );
 }
